936350a5ef9abbe299141b67785898c5509f7195
VinceYoon <vince.yoon01@gmail.com>
2021-08-05 11:30:11 -0700

    Add User Model

---
+++ users/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "dob",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+    list_filter = UserAdmin.list_filter + ("superhost",)
+
+    list_display = (
+        "username",
+        "email",
+        "gender",
+        "language",
+        "currency",
+        "superhost",
+    )
---
+++ users/models.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """Custom User Model"""
+
+    GENDER_CHOICE = (
+        ("male", "Male"),
+        ("female", "Female"),
+        ("other", "Other"),
+    )
+
+    LANGUAGE_CHOICE = (
+        ("en", "English"),
+        ("kr", "Korean"),
+    )
+
+    CURRENCY_CHOICE = (
+        ("usd", "USD"),
+        ("cad", "CAD"),
+        ("krw", "KRW"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICE, max_length=6, blank=True)
+    bio = models.TextField(blank=True)
+    dob = models.DateField(blank=True, null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, blank=True, default="en"
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, blank=True, default="cad"
+    )
+    superhost = models.BooleanField(default=False)

9c0b4c02cfe3e11324c5d52224782814c63760f4
VinceYoon <vince.yoon01@gmail.com>
2021-08-06 12:01:48 -0700

    Add Wish List Model

---
+++ lists/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.List)
+class ListAdmin(admin.ModelAdmin):
+    """List Admin"""
+
+    list_display = (
+        "name",
+        "user",
+    )
+
+    search_fields = ("name",)
+    filter_horizontal = ("rooms",)

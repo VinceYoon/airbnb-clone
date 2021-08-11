5d81fd6b997916b3ef994b785fb5794d1dce76c5
VinceYoon <vince.yoon01@gmail.com>
2021-08-11 15:34:16 -0700

    create a HomeView

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import redirect
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+    """HomeView"""
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+    allow_empty = True
+
+    def dispatch(self, *args, **kwargs):
+        try:
+            response = super(HomeView, self).dispatch(*args, **kwargs)
+            response.render()
+        except Exception:
+            return redirect("/")
+        return response
